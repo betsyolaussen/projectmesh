bc10e2e7d9eef384bc0cc3a4c40e9f8369a4e8a7
Betsy Olaussen <betsyolaussen@icloud.com>
2019-10-22 15:33:09 -0500

    updates

--- main.js
+++
@@ -1,6 +0,0 @@
-var logosmall = document.querySelector(".logosmall");
-document.body.classList.add('js-loading');
-img.addEventListener("load", removeLoadingClass);
-function removeLoadingClass() {
-  document.body.classList.remove('js-loading');
-}
