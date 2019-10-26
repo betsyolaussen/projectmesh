b3546cfd5c4a8290c989fc33aa82025d006430d6
Betsy Olaussen <betsyolaussen@icloud.com>
2019-10-26 09:01:32 -0500

    updates

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var myFont;
+
+function preload() {
+    myFont = loadFont('font/RubikMonoOne-Regular');
+}
+
+/*SKETCH 1*/
+var s = function (p) {
+    var x = 500;
+    var y = 100;
+    p.setup = function(){
+        p.createCanvas(500, 50);
+        
+    };
+
+    p.draw = function() {
+        p.text("PROBLEM STATEMENT", 0, 30);
+        p.textSize(32);
+        p.textFont('myFont');
+    };
+};
+
+    var myp5 = new p5(s, 'c1');
+
+    /*SKETCH 2*/
